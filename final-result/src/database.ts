84ab470bf911966fca827871a00031f34b84bc7c
RenanJPaula <renanjohannsen@gmail.com>
2021-09-01 10:57:29 -0300

    Adição da configuração via variáveis de ambiente do database

--- final-result/src/database.ts
+++ final-result/src/database.ts
@@ -1,8 +1,8 @@
 
+import config from 'config';
 import { Pool } from 'pg';
 
-const connectionString = 'your_connection_string';
-
+const connectionString = config.get<string>('database.uri');
 const db = new Pool({ connectionString });
 
 export default db;
