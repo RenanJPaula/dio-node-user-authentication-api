4c15d26480583aa7a058e0489f47df9a628fd7bd
RenanJPaula <renanjohannsen@gmail.com>
2021-09-06 21:07:59 -0300

    Adicionando o typescript e incrementando a forma de desenvolver

--- live-01/src/index.ts
+++ live-01/src/index.ts
@@ -1,2 +1,12 @@
 
-console.log('Executei!');
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'sucesso total! vocês são demais!!!' });
+});
+
+app.listen(3000, () => {
+    console.log('Aplicação executando na porta 3000!');
+});
